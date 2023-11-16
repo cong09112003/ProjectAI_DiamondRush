a448d6d31425ac0cde07eebd9890bd009ad7a321
cong09112003 <congnguyenhuu2703@gmail.com>
2023-11-16 09:13:45 +0700

    update single mode dealblock

--- singlemode.py
+++ singlemode.py
@@ -13,6 +13,8 @@ SPACE = " "
 BOX = "$"
 BINGO = "*"
 WALL = "#"
+
+window_size = (1000, 600)
   
 class SokobanGame:
     def __init__(self, map_file):
@@ -20,20 +22,56 @@ class SokobanGame:
         self.targets = []
         self.move_history = []
         self.aiStep = []
+        self.playerStep=[]
         self.listMappath = ["map/game01.txt","map/game02.txt","map/game03.txt"]
         self.curMappath = map_file
+        self.DeadLocks=[]
         pygame.init()  
         pygame.font.init()  
         self.load_map(map_file)
         self.button_font = pygame.font.Font(None, 36)
         self.step= len(self.move_history)
-
+        self.state="..."
+        if self.is_success==True:
+            self.state="Win"
     def load_map(self, map_file):
         with open(map_file, 'r') as f:
             for line in f.read().splitlines():
                 self.board.append(list(line))
         self.init_targets()
-
+        print(f"targets:{self.targets}")
+        self.init_deadblock()
+        print(f"dealocks:{self.DeadLocks}")
+    def is_success(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] == TARGET:
+                    return False
+        return True
+    # i!=0 and j!=0 and i!=len(self.board[0])-1 and j!=len(self.board[1])-1 and
+    # ((self.board[i-1][j]==WALL and self.board[i][j-1]==WALL) or
+    # (self.board[i-1][j]==WALL and self.board[i][j+1]==WALL) or
+    # (self.board[i+1][j]==WALL and self.board[i][j-1]==WALL) or
+    # (self.board[i+1][j]==WALL and self.board[i][j+1]==WALL))
+    def init_deadblock(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if  self.board[i][j] == SPACE or self.board[i][j] == PLAYER :
+                    # print(i,j)
+                    if i-1 >= 0 and j-1 >= 0 and i+1 <= len(self.board) and j+1 <= len(self.board[0]):
+                        if self.board[i-1][j]==WALL and self.board[i][j-1]==WALL:
+                            self.DeadLocks.append((i,j))  
+                        elif self.board[i-1][j]==WALL and self.board[i][j+1]==WALL:
+                            self.DeadLocks.append((i,j))  
+                        elif self.board[i+1][j]==WALL and self.board[i][j-1]==WALL:
+                            self.DeadLocks.append((i,j))
+                        elif self.board[i+1][j]==WALL and self.board[i][j+1]==WALL:
+                            self.DeadLocks.append((i,j))                     
+    def is_deadblock(self,row,col):
+        for dealbock in self.DeadLocks:
+            if dealbock[0] == row and dealbock[1] == col:
+                return True
+        return False
     def init_targets(self):
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
@@ -69,15 +107,19 @@ class SokobanGame:
 
     def move_left(self):
         self.move_player(0, -1)
+        self.playerStep.append('L')
 
     def move_right(self):
         self.move_player(0, 1)
+        self.playerStep.append('R')
 
     def move_up(self):
         self.move_player(-1, 0)
+        self.playerStep.append('U')
 
     def move_down(self):
         self.move_player(1, 0)
+        self.playerStep.append('D')
 
     def move_player(self, i, j):
         row, col = self.get_player_position()
@@ -152,13 +194,16 @@ class SokobanGame:
         if index > 0:
             self.curMappath = self.listMappath[index - 1]
             self.load_map_from_file(self.curMappath)
+    def printStep(self):
+        print (f"AI Step:{self.aiStep}")
+        print (f"Player step: {self.playerStep}")
 def main():
     #Cưả sổ chính
     game = SokobanGame("map/game01.txt")
     pygame.init()
     pygame.display.init()
     pygame.display.set_caption("Sokoban")
-    window_size = (1000, 450)
+    
     screen = pygame.display.set_mode(window_size)
     screen.fill((41,41,41))
 
@@ -209,19 +254,26 @@ def main():
             elif event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        if game.is_success(): 
+            game.state = 'win'
+            game.printStep()
+            
         game.draw_board(game_surface)
         screen.fill((41, 41, 41), (0, game_surface.get_height(), window_size[0], 50))
         steps_label = font.render(f"Steps: {game.step}", True, (255, 255, 255))
         time_label = font.render(f"Time: 0s", True, (255, 255, 255))
 
+        
+        state_label=font.render(f"State: {game.state}", True, (255, 255, 255))
         screen.blit(steps_label, (80, game_surface.get_height() + 10))
         screen.blit(time_label, (360, game_surface.get_height() + 10))
+        screen.blit(state_label, (500, game_surface.get_height() + 10))
         screen.blit(game_surface, (0, 0))
 
         pygame.display.flip()
         pygame_widgets.update(events)
         pygame.display.update()
-
+    
 def bfs():
     messagebox.showinfo("Hello", "BFS solve!")
 def home():
@@ -232,3 +284,4 @@ def home():
         print(f"Error opening Menu.py: {e}")
 if __name__ == "__main__":
     main()
+    
